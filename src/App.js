cf75bd0e409e88a8064e5651b2aca9a1aaf62cfe
EmaciatedStudent <yana.laperdina@gmail.com>
2022-01-06 20:13:06 +0500

    added app component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
 import React from "react";
 
+class App extends React.Component {
+    render () {
+        return (
+            <div className="App">
+                <div className="taskInput">
+                    <input type={"text"} placeholder="Введите задачу"></input>
+                    <input type={"button"}></input>
+                </div>
+                <select className="taskType">
+                    <option selected>Все задачи</option>
+                    <option>Текущие задачи</option>
+                    <option>Выполненные задачи</option>
+                </select>
+                <div className="taskBoard">
+
+                </div>
+            </div>
+        )
+    }
+}
+
 export default App;
